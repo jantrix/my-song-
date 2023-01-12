da0d71f290a05a1f709cc7200ce43f5b6eed8c10
jantrix <122298091+jantrix@users.noreply.github.com>
2023-01-12 12:15:09 +0530

    Add files via upload

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,52 @@
+import React from "react";
+//import { playAudio } from "../util";
+
+const LibrarySong = ({
+    name,
+    artist,
+    cover,
+    id,
+    setCurrentSong,
+    songs,
+    audioRef,
+    isPlaying,
+    setSongs,
+    active,
+}) => {
+    const songSelectHandler = async () => {
+        const selectedSong = songs.filter((state) => state.id === id);
+        await setCurrentSong({ ...selectedSong[0] });
+        //Set Active in library
+        const newSongs = songs.map((song) => {
+            if (song.id === id) {
+                return {
+                    ...song,
+                    active: true,
+                };
+            } else {
+                return {
+                    ...song,
+                    active: false,
+                };
+            }
+        });
+        setSongs(newSongs);
+
+        //Play audio
+        if (isPlaying) audioRef.current.play();
+    };
+    return (
+        <div
+            onClick={songSelectHandler}
+            className={`library-song ${active ? "selected" : ""}`}
+        >
+            <img src={cover} alt="" />
+            <div className="song-description">
+                <h3>{name}</h3>
+                <h4>{artist}</h4>
+            </div>
+        </div>
+    );
+};
+
+export default LibrarySong;
